d3c8ab341a9a4005b12518943ceec3cb4ca2000b
o2plusgithub <79360259+o2plusgithub@users.noreply.github.com>
2021-03-02 23:49:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -43,11 +43,7 @@ app.use(expressip().getIpInfoMiddleware);
 app.set('view engine', 'ejs');
 
 //app.use(helmet());
-app.use(
-  helmet({
-    contentSecurityPolicy: false,
-  })
-);
+
 
 app.use(express.static(__dirname + '/views'));
 
