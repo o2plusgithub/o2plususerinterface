34a72f18eee7c65d58964f6c412d7675872bf384
o2plusgithub <79360259+o2plusgithub@users.noreply.github.com>
2021-03-02 22:46:22 +0300

    test

---
+++ views/js/error.js
@@ -0,0 +1,8 @@
+
+  var form  = $("#loginjs");
+  form.addClass("ani-ring");
+  setTimeout(function(){
+    form.removeClass("ani-ring");
+  }, 2000);
+  $("#loginjs").fadeIn(1000);
+  $("#lockjs").fadeIn(2500);
