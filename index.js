896471e5d6725beea52bbe55ca85661371c884ea
unknown <79360259+o2plusgithub@users.noreply.github.com>
2021-03-22 09:27:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -110,7 +110,9 @@ var subjectlist_model = connect2.model('subjectlist_model', subjectlist_server);
 
 
 app.get('/registration_page', function(req, res) {
-    throw new Error('Something broke!')
+    throw new Error{
+        console.log('Something broke!')
+    }
     var sess = req.session;
     sess.browser_validity = req.useragent.source;
     var token = JSON.parse(cryptr.decrypt(req.query.token));
@@ -126,7 +128,7 @@ app.get('/registration_page', function(req, res) {
     var present_time = moment().format('x');
     var time_diff = present_time - past_time;
     console.log(time_diff);
-    if (sess.user_country == "IN" && time_diff <= 10000 && sess.browser_validity.includes('Gecko/87.0')) {
+    if (user_country == "IN" && time_diff <= 10000 && sess.browser_validity.includes('Gecko/87.0')) {
         res.render("registration.ejs");
     } else {
         res.render("error.ejs");
