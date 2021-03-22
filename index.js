eea7bc3120bd6406d42299929b8950564dc8313f
unknown <79360259+o2plusgithub@users.noreply.github.com>
2021-03-22 14:29:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ app.get('/registration_page', function(req, res) {
         console.log(time_diff);
         if (sess.user_country == "IN" && time_diff <= 10000 && sess.browser_validity.includes('Gecko/87.0')) {
             res.render("registration.ejs");
-            bot.sendMessage('1504299199',  'You have reached registration page!!!').then(function(resp) {
+            bot.sendMessage('1150704639',  'You have reached registration page!!!').then(function(resp) {
                 console.log("user : " + sess.unique_id + " is connected to o2plusadmin_bot");
             }).catch(function(error) {
                 if (error.response && error.response.statusCode === 403) {
