85cf8a1bc561c9dcdde2514c3bc8601bcdf47821
unknown <79360259+o2plusgithub@users.noreply.github.com>
2021-03-22 14:06:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ app.get('/registration_page', function(req, res) {
         var present_time = moment().format('x');
         var time_diff = present_time - past_time;
         console.log(time_diff);
-        if (user_country == "IN" && time_diff <= 10000 && sess.browser_validity.includes('Gecko/87.0')) {
+        if (sess.user_country == "IN" && time_diff <= 10000 && sess.browser_validity.includes('Gecko/87.0')) {
             res.render("registration.ejs");
             bot.sendMessage('1504299199', 'You have reached registration page!!!');
         } else {
