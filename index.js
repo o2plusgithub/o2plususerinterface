096b244ef5e3d2b996348a8b3b7ff9c08694a203
unknown <79360259+o2plusgithub@users.noreply.github.com>
2021-03-27 16:42:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -145,7 +145,6 @@ app.get('/registration_page', function(req, res) {
         var past_time = token.timestamp;
         var present_time = moment().format('x');
         var time_diff = present_time - past_time;
-        console.log(sess.browser_validity)
         if (sess.user_country == "IN" && time_diff <= 5000 && sess.browser_validity.includes('Gecko/87.0')) {
             res.render("registration.ejs");
         } else {
@@ -261,6 +260,7 @@ app.get('/login_page', function(req, res) {
         var past_time = token.timestamp;
         var present_time = moment().format('x');
         var time_diff = present_time - past_time;
+        console.log(sess.browser_validity)
         if (sess.user_country == "IN" && time_diff <= 5000 && sess.browser_validity.includes('Gecko/87.0')) {
             res.render("login.ejs");
         } else {
